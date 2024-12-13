6c5e98c19ad27947bdc3560e7b3dc48485b82e56
Roadie Engineer <engineering@roadie.io>
2024-12-13 11:36:14 -0500

    Update .NET version to 8 and add Sentry.io

--- Program.cs
+++ Program.cs
@@ -2,18 +2,58 @@ namespace TeleprompterConsole;
 
 public class Program
 {
+    
     public static async Task Main(string[] args)
     {
+        // Initialize the Sentry SDK.  (It is not necessary to dispose it.)
+        SentrySdk.Init(options =>
+        {
+            // You can set here in code, or you can set it in the SENTRY_DSN environment variable.
+            // See https://docs.sentry.io/product/sentry-basics/dsn-explainer/
+            options.Dsn = "https://f1737c236ec81613cb16e853ec82f4f5@o4508461602439168.ingest.de.sentry.io/4508461610893392";
+
+            // When debug is enabled, the Sentry client will emit detailed debugging information to the console.
+            // This might be helpful, or might interfere with the normal operation of your application.
+            // We enable it here for demonstration purposes.
+            // You should not do this in your applications unless you are troubleshooting issues with Sentry.
+            options.Debug = true;
+
+            // This option is recommended, which enables Sentry's "Release Health" feature.
+            options.AutoSessionTracking = true;
+
+            // This option is recommended for client applications only. It ensures all threads use the same global scope.
+            // If you are writing a background service of any kind, you should remove this.
+            options.IsGlobalModeEnabled = true;
+
+            // This option tells Sentry to capture 100% of traces. You still need to start transactions and spans.
+            options.TracesSampleRate = 1.0;
+
+            
+        });
+
+
+        // This starts a new transaction and attaches it to the scope.
+        var transaction = SentrySdk.StartTransaction("Program Main", "function");
+        SentrySdk.ConfigureScope(scope => scope.Transaction = transaction);
+
         await RunTeleprompter();
+
+        // Always try to finish the transaction successfully.
+        // Unhandled exceptions will fail the transaction automatically.
+        // Optionally, you can try/catch the exception, and call transaction.Finish(exception) on failure.
+        transaction.Finish();
     }
 
     private static async Task RunTeleprompter()
     {
+        SentrySdk.CaptureMessage("Something went wrong");
+
         var config = new TelePrompterConfig();
         var displayTask = ShowTeleprompter(config);
 
         var speedTask = GetInput(config);
         await Task.WhenAny(displayTask, speedTask);
+
     }
 
     private static async Task ShowTeleprompter(TelePrompterConfig config)
